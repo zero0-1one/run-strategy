ac99814ba2293c4c1a4e369f46b4492900e6cdd9
ygy <ygy358585315@163.com>
2025-10-22 14:08:38 +0800

    chore: 去掉不用的依赖包

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import jest from 'eslint-plugin-jest';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import jest from "eslint-plugin-jest";
 
-export default tseslint.config(
+export default [
   {
-    ignores: ['eslint.config.js'],
+    ignores: ["eslint.config.js"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -20,7 +20,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: "commonjs",
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -29,52 +29,52 @@ export default tseslint.config(
   },
   {
     rules: {
-      'prettier/prettier': [
-        'error',
+      "prettier/prettier": [
+        "error",
         {
           printWidth: 120,
           singleQuote: true,
-          trailingComma: 'all',
+          trailingComma: "all",
         },
       ],
-      quotes: ['error', 'single', { avoidEscape: true }],
-      'no-console': 'warn',
-      'no-multi-spaces': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-unsafe-argument': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
+      quotes: ["error", "single", { avoidEscape: true }],
+      "no-console": "warn",
+      "no-multi-spaces": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-unsafe-argument": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
         {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
         },
       ],
 
-      '@typescript-eslint/explicit-member-accessibility': [
-        'error',
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
         {
-          accessibility: 'explicit',
+          accessibility: "explicit",
         },
       ],
 
-      'no-multiple-empty-lines': [
-        'error',
+      "no-multiple-empty-lines": [
+        "error",
         {
           max: 2,
           maxEOF: 0,
           maxBOF: 0,
         },
       ],
-      'jest/no-disabled-tests': 'warn',
-      'jest/no-focused-tests': 'error',
-      'jest/no-identical-title': 'error',
-      'jest/prefer-to-have-length': 'warn',
-      'jest/valid-expect': 'error',
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
     },
   },
   {
-    ignores: ['./node_modules', './dist', './coverage', './schema'],
+    ignores: ["./node_modules", "./dist", "./coverage", "./schema"],
   },
-);
+];
